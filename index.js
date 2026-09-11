617d39f8d0ff01098767e1c389d491034f8add06
Sameer <rydhans@yahoo.com>
2026-09-11 11:47:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// Target 1: Arbitrary File Read
+app.get('/read', (req, res) => {
+  const file = req.query.file;
+  fs.readFile(file, 'utf8', (err, data) => {
+    res.send(data);
+  });
+});
+
+// Target 2: Unvalidated arithmetic
+app.get('/add', (req, res) => {
+  const result = parseInt(req.query.a) + parseInt(req.query.b);
+  res.json({ result });
+});
+app.listen(3000);
